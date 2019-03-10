83045f8788ec443f2cc3c068c07133d9f46b6bcf
reverienest <44439363+reverienest@users.noreply.github.com>
2019-03-10 01:15:45 -0500

    Update GameState.cs

--- Project Chronograph Test/Assets/Scripts/GameState.cs
+++ Project Chronograph Test/Assets/Scripts/GameState.cs
@@ -2,105 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameState : MonoBehaviour {
+public class GameState : MonoBehaviour
+{
 
     //Create states for game
     public enum GameStates
     {
-        playing = 0,
-        menu = 1,
-        loading = 2,
-        scene = 3,
-        script = 4
+        None = 0,
+        Playing = 1,
+        Menu = 2,
+        Loading = 3
     }
 
-    public GameStates currentState = GameStates.playing;
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    public GameStates currentState = GameStates.Playing;
+
+    // Use this for initialization
+    void Start()
     {
-		switch(currentState)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region None
+
+    IEnumerator None_EnterState()
+    {
+        while(true)
         {
-            case GameStates.playing: 
-                //The level is in session
-                //Check all necessary switches and camera work
-                if (Input.GetKeyDown(KeyCode.Return))
-                    {
-                    Debug.Log("Game state in \"playing\"");
-                    }
-                if (Input.GetKeyDown(KeyCode.Escape))
-                    {
-                    currentState = GameStates.menu;
-                    Debug.Log("Switching to \"menu\"");
-                }
-                if (Input.GetKeyDown(KeyCode.Backspace))
-                    {
-                    currentState = GameStates.loading;
-                    Debug.Log("Switching to \"loading\"");
-                }
-                if (Input.GetKeyDown(KeyCode.Tab))
-                    {
-                    currentState = GameStates.scene;
-                    Debug.Log("Switching to \"scene\"");
-                }
-                if (Input.GetKeyDown(KeyCode.Delete))
-                {
-                    currentState = GameStates.script;
-                    Debug.Log("Switching to \"script\"");
-                }
-                break;
-            case GameStates.menu:
-                //The menu is open
-                //Toggle buttons and animations
-                if (Input.GetKeyDown(KeyCode.Return))
-                    {
-                    Debug.Log("Game state in \"menu\"");
-                    }
-                if (Input.GetKeyDown(KeyCode.Escape))
-                    {
-                    currentState = GameStates.playing;
-                    }
-                break;
-            case GameStates.loading:
-                //The game is loading a new level
-                //Load a level transition
-                if (Input.GetKeyDown(KeyCode.Return))
-                    {
-                    Debug.Log("Game state in \"loading\"");
-                    }
-                if (Input.GetKeyDown(KeyCode.Backspace))
-                {
-                    currentState = GameStates.playing;
-                }
-                break;
-            case GameStates.scene:
-                //The game is in a cutscene
-                //Play presentation
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    Debug.Log("Game state in \"scene\"");
-                }
-                if (Input.GetKeyDown(KeyCode.Backspace))
-                {
-                    currentState = GameStates.playing;
-                }
-                break;
-            case GameStates.script:
-                //The game is playing a scripted event
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    Debug.Log("Game state in \"script\"");
-                }
-                if (Input.GetKeyDown(KeyCode.Backspace))
-                {
-                    currentState = GameStates.playing;
-                }
-                break;
+            //Halt everything.
         }
-	}
+    }
+
+    #endregion
+
+    #region Playing
+
+    IEnumerator Playing_EnterState()
+    {
+        while (true)
+        {
+            //Check things baby.
+        }
+
+        //Function for detecting scene playing
+        //turn off when happening
+    }
+
+    #endregion
+
+    #region Menu
+    IEnumerator Menu_EnterState()
+    {
+        //Open up the menu 
+        while (true)
+        {
+            //Menu stuff
+        }
+    }
+
+    #endregion
+
+    #region Loading
+
+    IEnumerator Loading_EnterState()
+    {
+        //Play a level transition and load the next level.
+        while(true)
+        {
+            //Transition
+        }
+    }
+
+    #endregion
+
 }
