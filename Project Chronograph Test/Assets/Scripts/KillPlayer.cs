58731ee64dd05bbb5cf719d1c3b02422cff92117
reverienest <44439363+reverienest@users.noreply.github.com>
2019-04-10 01:38:01 -0400

    KillPlayer

---
+++ Project Chronograph Test/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour {
+
+    public PlayerController player;
+    public Animator transitionsAnim;
+    public string levelName;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            StartCoroutine(ResetLevel());
+        }
+    }
+
+    IEnumerator ResetLevel()
+    {
+            player.enabled = false;
+            //play death animation
+            transitionsAnim.SetTrigger("end2"); //play a level reset transition, not end2. 
+            yield return new WaitForSeconds(1.5f);
+            player.enabled = true;
+            SceneManager.LoadScene(levelName);
+    }
+}
